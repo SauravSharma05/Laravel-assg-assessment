50d7a21e3c9b410863ee29920d35047fc560c528
Saurav Sharma <saurav5122000@gmail.com>
2023-12-26 12:38:28 +0530

    assessment updation

--- Assessment/Musicana/resources/views/register.blade.php
+++ Assessment/Musicana/resources/views/register.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
+        <title>Registration page</title>
     </head>
 <body>
     <h1>registration page</h1>
